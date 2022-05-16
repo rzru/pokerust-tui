6a9cedf1dd3eddf546693b0eda3d792853e0f8a2
rzru <razzaru@yandex.ru>
2022-05-16 13:14:46 +0300

    :white_check_mark: add tests to named_api_resource.rs model

--- src/models/named_api_resource.rs
+++ src/models/named_api_resource.rs
@@ -11,3 +11,26 @@ impl NamedApiResource {
         self.name.as_ref().unwrap_or(&"".to_string()).to_string()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::NamedApiResource;
+
+    fn get_stub_named_api_resource(name: Option<String>) -> NamedApiResource {
+        NamedApiResource { name, url: None }
+    }
+
+    #[test]
+    fn named_api_resource_get_name_or_stub_with_name() {
+        let named_api_resource = get_stub_named_api_resource(Some(String::from("test")));
+
+        assert_eq!(named_api_resource.get_name_or_stub(), String::from("test"))
+    }
+
+    #[test]
+    fn named_api_resource_get_name_or_stub_without_name() {
+        let named_api_resource = get_stub_named_api_resource(None);
+
+        assert_eq!(named_api_resource.get_name_or_stub(), String::from(""))
+    }
+}
